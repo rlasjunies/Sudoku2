49257d2a0a37e95ec8c93e646587a564175ebb4f
Richard Lasjunies <rlasjunies@gmail.com>
2021-05-13 09:16:10 +0200

    Cross feature effect - load weather each 10 click

---
+++ Store/WeatherCounter/Effect_LoadForecast10nthOfCounter.cs
@@ -0,0 +1,40 @@
+using Fluxor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Components;
+using CounterStore = Sudoku.Store.Counter;
+using WeatherStore = Sudoku.Store.Weather;
+
+
+namespace Sudoku.Store.Weather
+{
+    // public class FetchForeCastsDataEffect : Effect<Actions.LoadForecastsAction>
+    public class WeatherCounterEffect
+    {
+        private readonly HttpClient Http;
+        private readonly IState< CounterStore::StateCounter> CounterState;
+
+        public WeatherCounterEffect(HttpClient http, IState<CounterStore::StateCounter> counterState)
+        {
+            Http = http;
+            CounterState = counterState;
+        }
+
+        [EffectMethod(typeof(CounterStore::Actions.IncrementCounter))]
+        public async Task  ReloadForecastEvery10Increment(IDispatcher dispatcher)
+        {
+            // every tenth increment
+            if (CounterState.Value.ClickCount % 10 == 0)
+            {
+                dispatcher.Dispatch(new WeatherStore::Actions.LoadForecasts());
+            } 
+
+            // trick: without this line there is compiler error 
+            await Task.CompletedTask;
+        }
+    }
+}
