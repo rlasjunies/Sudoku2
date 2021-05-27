6e4f581cb896c14b666c766f47019340a96be16e
Richard Lasjunies <rlasjunies@gmail.com>
2021-05-27 19:54:37 +0200

    component AccPage

---
+++ SudokuBlazor/Components/AccPage.razor..cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Sudoku.Components
+{
+    public partial class AccPageBase : AccComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+    }
+}
