3c60d1e5447b1f68943b0a25ccf11433b82cc6df
Richard Lasjunies <rlasjunies@gmail.com>
2021-05-27 19:45:20 +0200

    add array helper

--- Helper/Array.cs
+++ Helper/Array.cs
@@ -26,9 +26,9 @@ Start from the last element
 Randomly select one element of the array and swap it with the last element
 Take the array with size decreased in one and repeat the whole process till you reach the first element.
 */
-    public static int[] Shuffle_knuthfisheryates2(this int[] arr)
+    public static T[] Shuffle_knuthfisheryates2<T>(this T[] arr)
     {
-        var temp = arr.Length;
+        //var temp = arr.Length;
         // var j = arr.Length;
         var lastIndex = arr.Length;
         while (lastIndex > 1)
@@ -38,11 +38,33 @@ Take the array with size decreased in one and repeat the whole process till you 
             // var rand = new Random().Next(100);
             // j = ~~(randomIndex * (i + 1));
             // Console.WriteLine($"lastIndex:{lastIndex} rand:{randomIndex}");
-            temp = arr[lastIndex];
+            var temp = arr[lastIndex];
             arr[lastIndex] = arr[randomIndex];
             arr[randomIndex] = temp;
         }
 
         return arr;
     }
+
+    public static T[] ClonePush<T>(this T[] arr, T val)
+    {
+        T[] temp = new T[arr.Length];
+        Array.Copy(arr, temp, arr.Length);
+        Array.Resize(ref temp, temp.Length + 1);
+        //temp[temp.Length - 1] = val;
+        temp[^1] = val;
+        return temp;
+    }
+
+    public static T[] Filter<T>(this T[] arr, Predicate<T> P)
+    {
+        T[] temp = Array.Empty<T>();
+        foreach (var val in arr)
+        {
+            // do not push if predicate is true
+            if (!P(val)) temp = temp.ClonePush<T>(val);
+        }
+        return temp;
+    }
+
 }
